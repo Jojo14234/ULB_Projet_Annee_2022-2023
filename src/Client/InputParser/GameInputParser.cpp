75346cb821817494d562e967c0f77728490cf077
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-02-19 13:17:26 +0000

    parse method modified

--- src/Client/InputParser/GameInputParser.cpp
+++ src/Client/InputParser/GameInputParser.cpp
@@ -2,5 +2,12 @@
 
 void GameInputParser::parse() {
     InputParser::parse();
+    const std::string &query = (*this)[0];
+
+    if ( query == "/roll") { this->query_type = GAME_QUERY_TYPE::ROLL_DICE; }
+
+    
+
+    else if ( query == "/disconnect" ) { this->query_type = GAME_QUERY_TYPE::LEAVE; }
 
 }
