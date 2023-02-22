53a099c574cd61e6d8b94752c5af016c647d1049
wewewen <93323493+h4w3n@users.noreply.github.com>
2023-02-22 10:24:34 +0000

    constructeur des classes un peu

--- src/Game/Card.cpp
+++ src/Game/Card.cpp
@@ -1,5 +1,40 @@
 #include "Card.hpp"
 
-class Card{
-	//test commit depuis le code space
+LuckyCardDeck::LuckyCardDeck(){
+    for (size_t i = 0; i < money_card_n0.size(); ++i){
+        this->card_list[i] = CardMoney(money_card_nO[i]);
+    }
+    for (size_t i = 0; i < cell_card_n0.size(); ++i){
+        this->card_list[i] = CardCell(money_card_nO[i]);
+    }
 }
+
+//-------------------------------
+CommunityCardDeck::CommunityCardDeck(){
+    
+}
+
+//###################################################
+Card::Card(int card_nO){
+    Json::Value root;
+	Json::Reader reader;
+	std::ifstream file("data/card_data.json");
+	if (not reader.parse(file, root)) { perror("Error parsing file"); return; }
+
+    this->description = (card_nO <= 20) ? root["MoneyCard"][card_nO]["descript"] : root["CellCard"][card_nO]["descript"] ;
+}
+
+//-------------------------------
+CardCell::CardCell(int card_nO){
+    Json::Value root;
+	Json::Reader reader;
+	std::ifstream file("data/card_data.json");
+	if (not reader.parse(file, root)) { perror("Error parsing file"); return; }
+
+    this->dest = root[CellCard][card_nO]["dest"] ;
+    this->gain_money = root[CellCard][card_nO]["money"] ;
+}
+
+//-------------------------------
+
+//-------------------------------
--- src/Game/Card.hpp
+++ src/Game/Card.hpp
@@ -6,39 +6,47 @@ class CardDeck{
 protected:
 	std::array<std::shared_ptr<Card>, 16> card_list;
 public:
+	
 
+};
 
-}
-
-class CommunityCardDeck: public CardDeck {
-public:
-	CommunityCardDeck();
-}
-
+//-------------------------------
 class LuckyCardDeck: public CardDeck {
+	std::array<int, 16> money_card_nO = [1,2,3,4,5,6,7,8];
+	std::array<int, 16> cell_card_nO = [21,22,23,24,25,26];
 public:
 	LuckyCardDeck();
-}
+};
+
+//-------------------------------
+class CommunityCardDeck: public CardDeck {
+	std::array<int, 16> money_card_nO = [1,2,3,4,5,6,7,8];
+	std::array<int, 16> cell_card_nO = [21,22,23,24,25,26];
+public:
+	CommunityCardDeck();
+};
 
 
-//-----------------------------------------------
+
+//###################################################
 class Card{
 protected:
 	std:string description;
 public:
-	void action();
-}
+	Card(int card_nO);
+	void action(Player player);
+};
 
-
+//-------------------------------
 class CardCell: public Card {
 private:
 	int dest;
 	bool gain_money;
 public: 
-	void action();
-}
+	void action(Player player);
+};
 
-
+//-------------------------------
 class CardMoney: public Card{
 private:
 	int amount;
@@ -46,11 +54,11 @@ private:
 	int amount_hotel;
 	
 public: 
-	void action();
-}
+	void action(Player player);
+};
 
-
+//-------------------------------
 class JailCard: public Card{
 public: 
-	void action();
-}
+	void action(Player player);
+};
