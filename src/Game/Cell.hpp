93caa20c68a59fddf4202493060dd300727df7e4
wewewen <93323493+h4w3n@users.noreply.github.com>
2023-02-23 14:43:37 +0000

    j'ai appliquer le SRD

--- src/Game/Cell.hpp
+++ src/Game/Cell.hpp
@@ -7,8 +7,64 @@
 
 
 class Cell {
-
+private:
+    int position;
+public:
+    Cell(int pos);
+    void action(Player* player);
 };
+//-------------------------------
+class PropertyCell: public Cell {
+private:
+    Property property;
+    bool isOwner();
+public: 
+    PropertyCell()
+    void action(Player* player)=0;
+}
+
+//-------------------------------
+class TaxCell: public Cell{
+private:
+    int price;
+    void takeMoney();
+public:
+    TaxCell();
+    void action(Player* player);
+}
+
+//-------------------------------
+class JailCell: public Cell{
+private:
+    void outWithMoney();
+    void outWIthCard();
+    void outWithDice();
+    void out();
+public:
+    JailCell();
+    void action(Player* player);
+}
+
+//-------------------------------
+class GoJailCell: public Cell{
+private:
+    JailCell* jail;
+    void goToJail();
+public: 
+    GoJailCell();
+    void action(Player* player);
+}
+
+//-------------------------------
+class DrawableCardCell: public{
+private:
+    CardDeck* deck;
+    void drawACard();
+public:
+    DrawableCardCell()
+    void action(Player* player);
+}
+
 
 
 #endif //INFO_F209_GR5_2022_CELL_HPP
--- src/Game/Property.hpp
+++ src/Game/Property.hpp
@@ -5,8 +5,26 @@
 #ifndef INFO_F209_GR5_2022_PROPERTY_HPP
 #define INFO_F209_GR5_2022_PROPERTY_HPP
 
+enum COLOR{BROWN, LIGHT_BLUE, PINK, ORANGE, RED, YELLOW, GREEN, DARK_BLUE};
+enum CELL_STATUS{PAID, FREE, HYPOTEK};
+enum PROPERTY_LEVEL{EMPTY, ONE, TWO, THREE, FOUR, HOTEL};
 
 class Property {
+private: 
+    COLOR color;
+    int purchase_price;
+    int rent_price;
+    CELL_STATUS status;
+    PROPERTY_LEVEL level;
+    Player* owner;
+
+    void purchase();
+    void rent();
+    void setLevel();
+
+public:
+    Property();
+    void action(Player* player);
 
 };
 
