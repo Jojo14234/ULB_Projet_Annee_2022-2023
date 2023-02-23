3cfd555ff3d4dd7ef1f4b757bb8950673127e96d
hugocharels <hugocharels@hotmail.com>
2023-02-23 21:13:33 +0000

    new folder

---
+++ src/ClientNcurse/Button.hpp
@@ -0,0 +1,24 @@
+#ifndef _CLIENT_BUTTON_HPP_
+#define _CLIENT_BUTTON_HPP_
+
+#include "utils.hpp"
+
+
+class Button {
+
+	 
+
+
+public:
+
+	Button();
+	~Button();
+
+	bool contains(int x, int y);
+
+
+
+
+};
+
+#endif // _CLIENT_BUTTON_HPP_
