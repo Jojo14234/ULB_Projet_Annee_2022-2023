62d499b58d5e971db47ec52dbe8a9498c455780f
hugocharels <hugocharels@hotmail.com>
2023-02-23 21:36:12 +0000

    new files

--- src/ClientNcurse/Button.hpp
+++ src/ClientNcurse/Button.hpp
@@ -1,23 +1,25 @@
 #ifndef _CLIENT_BUTTON_HPP_
 #define _CLIENT_BUTTON_HPP_
 
+#include <ncurses.h>
+#include <string>
+
 #include "utils.hpp"
 
 
-class Button {
+class Button : puclic Window {
 
-	 
-
+	Point pos;
+	Point size;
 
 public:
 
-	Button();
-	~Button();
+	Button( Point pos, Point size, std::string label):pos(pos), size(size) {}
 
-	bool contains(int x, int y);
-
-
-
+	bool isClicked(Point point) const {
+		return pos.x <= point.x && point.x <= pos.x + size.x &&
+			pos.y <= point.y && point.y <= pos.y + size.y;
+	}
 
 };
 
--- src/ClientNcurse/utils.hpp
+++ src/ClientNcurse/utils.hpp
@@ -1,2 +1,11 @@
+#ifndef _CLIENT_UTILS_HPP_
+#define _CLIENT_UTILS_HPP_
+
+struct Point {
+	int x;
+	int y;
+};
 
 
+
+#endif // _CLIENT_UTILS_HPP_
