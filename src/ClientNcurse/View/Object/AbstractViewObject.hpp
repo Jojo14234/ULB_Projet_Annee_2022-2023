d697130d2f1553ae43cbfae1a353db82a5ef6e0b
hugocharels <hugocharels@hotmail.com>
2023-02-25 14:02:05 +0000

    code done

--- src/ClientNcurse/View/Object/AbstractViewObject.hpp
+++ src/ClientNcurse/View/Object/AbstractViewObject.hpp
@@ -1,9 +1,22 @@
 #ifndef _ABSTRACT_VIEW_OBJECT_HPP
 #define _ABSTRACT_VIEW_OBJECT_HPP
 
+
+#include "ObjectInfo.hpp"
+
+
 class AbstractViewObject {
 
+protected:
+
+	ObjectInfo info;
+
 public:
+
+	AbstractViewObject(ObjectInfo info) : info(info) {}
+	AbstractViewObject(int x, int y, int width, int height) : info(x, y, width, height) {}
+
+	ObjectInfo getInfo() const { return info; }
 
 };
 
--- src/ClientNcurse/View/Object/ObjectInfo.hpp
+++ src/ClientNcurse/View/Object/ObjectInfo.hpp
@@ -1,9 +1,44 @@
 #ifndef _OBJECT_INFO_HPP
 #define _OBJECT_INFO_HPP
 
+
+
+class Point {
+	int x, y;
+public:
+	Point(int x, int y) : x(x), y(y) {}
+	int getX() const { return x; }
+	int getY() const { return y; }
+};
+
+class Size : public Point {
+public:
+	using Point::Point;
+};
+
+class Position : public Point {
+public:
+	using Point::Point;
+};
+
+
 class ObjectInfo {
 
+	Position pos;
+	Size size;
+
 public:
+
+	ObjectInfo(Position pos, Size size) : pos(pos), size(size) {}
+	ObjectInfo(int x, int y, int width, int height) : pos(x, y), size(width, height) {}
+
+	Position getPos() const { return pos; }
+	Size getSize() const { return size; }
+	
+	int getX() const { return pos.getX(); }
+	int getY() const { return pos.getY(); }
+	int getWidth() const { return size.getX(); }
+	int getHeight() const { return size.getY(); }
 
 };
 
