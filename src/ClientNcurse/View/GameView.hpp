5a3e8513c723b33343e42f0563dd40a2fdf97446
XEL0 <axelyx@gmail.com>
2023-03-03 22:58:38 +0100

    zone de chat (miaou)

--- src/ClientNcurse/View/GameView.hpp
+++ src/ClientNcurse/View/GameView.hpp
@@ -3,18 +3,31 @@
 
 #include "AbstractView.hpp"
 #include "Object/Board.hpp"
+#include "Object/InputButtonFrame.hpp"
+#include "Object/Text.hpp"
 
 
 class GameView : public AbstractView {
 
+	int up_margin = 5;
+	int left_margin = 8;
+
 	Board board;
+	Text chat_text{ObjectInfo{3,8, up_margin-2, 125+40-4}, {"chat"}};
+	Text console_text{ObjectInfo{3,16, up_margin+5+(4*4), 125+40-8}, {"console"}};
+	InputButtonFrame chat{{5+(4*4), 80, up_margin, 125}};
+	InputButtonFrame console{{5+(4*4), 80, up_margin+5+(4*4)+2, 125}};
 
 public:
 
-	GameView(Client *model, int player_nb) : AbstractView(model), board{{11, 11, 5, 8}, player_nb} {}
+	GameView(Client *model, int player_nb) : AbstractView(model), board{{11, 11, up_margin, left_margin}, player_nb} {}
 	
 	void draw() override {
 		board.draw();
+		chat.draw();
+		console.draw();
+		chat_text.draw();
+		console_text.draw();
 	}
 };
 
