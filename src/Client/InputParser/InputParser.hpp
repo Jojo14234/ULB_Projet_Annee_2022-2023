b2bed60d653f6cb28b9c53e531afdd15832d7ea8
Ryckeboer rémy <92799821+Remy-rkb@users.noreply.github.com>
2023-03-07 11:25:42 +0100

    Add doc for InputParser directory

--- src/Client/InputParser/InputParser.hpp
+++ src/Client/InputParser/InputParser.hpp
@@ -10,33 +10,51 @@ class InputParser {
 private:
 
 	const std::string input;
-	std::vector<std::string> input_splited;
-	
-	// Split the input to a list of string seperate by a ' '
+	std::vector<std::string> input_separated;
+
+   /*
+	* Belong to InputParser classe.
+    * This function splits the input string by space (' ') character and stores the resulting substrings in a vector of strings called `input_separated`.
+    * It clears the `input_separated` vector before splitting the input string.
+    */
     void split() {
-		input_splited.clear();
+        input_separated.clear();
 		std::string str;	
 		for ( char e : this->input ) { 
-			if ( e == ' ' ) { this->input_splited.push_back(str); str=""; }
+			if ( e == ' ' ) { this->input_separated.push_back(str); str=""; }
 			else { str += e; }
-		} this->input_splited.push_back(str);
+		} this->input_separated.push_back(str);
 	}
 
 protected:
 
-	// Parse the input from the client
+	/*
+	 * Belongs to InputParser
+	 * Called in the constructor.
+	 * Actually does not parse but, split the Input with InputParser::split();
+	 */
 	virtual void parse() { this->split(); }
 
 public:
 
+    /*
+    * Constructor of InputParser, take a string `Input` as parameters.
+    * It stores it in variable `input`. Splits and stores it vector `input_separated`
+    */
 	InputParser(const std::string &input) : input{input} { this->parse(); }
 	virtual ~InputParser()=default;
 
-	// The n parameter from the input_splited
-	const std::string &operator[](int n) const { return this->input_splited[n]; }
+    /*
+     * This code defines a method that provides a convenient way to access elements of the parsed input string vector by overloading the `[]` operator.
+     * The method takes an integer as a parameter and returns a constant reference to the corresponding element of the `input_separated` vector.
+     */
+	const std::string &operator[](int n) const { return this->input_separated[n]; }
 
-	// The nb of parameters (does not count the query type ex : /login remy rkb -> 2, not 3)
-	int getNbParameters() const { return input_splited.size() - 1; }
+    /*
+     * This function return the number of parameters in the input.
+     * It doesn't count the query field. (e.g. : /login [username] [password] have 2 parameters)
+     */
+	int getNbParameters() const { return input_separated.size() - 1; }
 
 };
 
--- src/Client/InputParser/MainInputParser.cpp
+++ src/Client/InputParser/MainInputParser.cpp
@@ -2,44 +2,59 @@
 
 
 // Private
-
 void MainInputParser::parse() {
 	InputParser::parse();
 	const std::string &query = (*this)[0];
-	if ( query == "/login" ) { this-> query_type = QUERY_TYPE::LOGIN; }
-	else if ( query == "/register" ) { this->query_type = QUERY_TYPE::REGISTER; }
-	else if ( query == "/join" ) { 
-		if (this->isValidCode()) { this->query_type = QUERY_TYPE::JOIN_GAME;}
-	}
-	else if ( query == "/message" or query == "/msg" ) {
-		if (this->getNbParameters() != 2) { return; }
-		if ( (*this)[1] == "show" ) {
-			this->query_type = QUERY_TYPE::MESSAGE_SHOW;	
-		} else {
-			this->query_type = QUERY_TYPE::MESSAGE_SEND;
-		}
-	}
-	else if ( query == "/create" ) { this->query_type = QUERY_TYPE::CREATE_GAME;}
-	else if ( query == "/disconnect" or query == "/d" ) { this->query_type = QUERY_TYPE::DISCONNECT; }
-	else if ( query == "/ranking" or query == "/r") {
-		if (this->getNbParameters() != 1) { return; }
-		else if ((*this)[1] == "top") { this->query_type = QUERY_TYPE::RANKING_TOP; }
-		else if ((*this)[1] == "pos") { this->query_type = QUERY_TYPE::RANKING_POS; }
-	}
-	else if ( query == "/friends" or query == "/f" ) {
-		if (this->getNbParameters() < 1) { return; }
-		else if ( (*this)[1] == "list" ) { this->query_type = QUERY_TYPE::FRIENDS_LIST; }
-		else if ( (*this)[1] == "request" ) { this->query_type = QUERY_TYPE::FRIENDS_REQUEST; }
-		else if ( (*this)[1] == "accept" ) { this->query_type = QUERY_TYPE::FRIENDS_ACCEPT; }
-		else if ( (*this)[1] == "refuse" ) { this->query_type = QUERY_TYPE::FRIENDS_REFUSE; }
-		else if ( (*this)[1] == "add" ) { this->query_type = QUERY_TYPE::FRIENDS_ADD; }
-		else if ( (*this)[1] == "remove" ) { this->query_type = QUERY_TYPE::FRIENDS_REMOVE; }
-	}
+    switch (query) {
+        case "/login":      this->query_type = QUERY_TYPE::LOGIN;       break;
+        case "/register":   this->query_type = QUERY_TYPE::REGISTER;    break;
+        case "/create":     this->query_type = QUERY_TYPE::CREATE_GAME; break;
+        case "/disconnect":
+        case "/d":          this->query_type = QUERY_TYPE::DISCONNECT;  break;
+        case "/join":       this->joinInput();                          break;
+        case "/message":
+        case "/msg":        this->messageInput();                       break;
+        case "/ranking":
+        case "/r":          this->rankingInput();                       break;
+        case "/friends":
+        case "/f":          this->friendsInput();                       break;
+        default:            break;
+    }
 }
 
-bool MainInputParser::isValidCode() {
-	std::string code = (*this)[1];
-	if (code.size() != 4) { return false; }
+bool MainInputParser::isValidCode(std::string code) {
+	if  (code.size() != 4)    { return false; }
 	for (auto c : code) { if (not isdigit(c)) return false; }
 	return true;
+}
+
+void MainInputParser::joinInput() {
+    if (this->isValidCode((*this)[1])) { this->query_type = QUERY_TYPE::JOIN_GAME; }
+    else { std::cout << "(MainInputParser::joinInput()) Le code n'est pas valide. " << std::endl;}
+}
+
+void MainInputParser::messageInput() {
+    if (this->getNbParameters() == 2) {
+        this->query_type = ((*this)[1] == "show") ? QUERY_TYPE::MESSAGE_SHOW : QUERY_TYPE::MESSAGE_SEND; }
+    else { std::cout << "(MainInputParser::messageInput()) Il n'y a pas exactement 2 arguments, il y en a [" << this->getNbParameters() << "]." << std::endl;}
+}
+
+void MainInputParser::rankingInput() {
+    if (this->getNbParameters() != 1) { return; }
+    else if ((*this)[1] == "top") { this->query_type = QUERY_TYPE::RANKING_TOP; }
+    else if ((*this)[1] == "pos") { this->query_type = QUERY_TYPE::RANKING_POS; }
+    else { std::cout << "(MainInputParser::rankingInput()) Format de la requête incorrect, essayez /ranking {top, pos}" << std::endl;}
+}
+
+void MainInputParser::friendsInput() {
+    if (this->getNbParameters() < 1)    { return; }
+    else if ( (*this)[1] == "list" )    { this->query_type = QUERY_TYPE::FRIENDS_LIST; }
+    else if ( (*this)[1] == "request" ) { this->query_type = QUERY_TYPE::FRIENDS_REQUEST; }
+    else if ( (*this)[1] == "accept" )  { this->query_type = QUERY_TYPE::FRIENDS_ACCEPT; }
+    else if ( (*this)[1] == "refuse" )  { this->query_type = QUERY_TYPE::FRIENDS_REFUSE; }
+    else if ( (*this)[1] == "add" )     { this->query_type = QUERY_TYPE::FRIENDS_ADD; }
+    else if ( (*this)[1] == "remove" )  { this->query_type = QUERY_TYPE::FRIENDS_REMOVE; }
+    else { std::cout << "(MainInputParser::friendsInput()) Aucune commande correspondante." << std::endl;
+           std::cout << "Essayez : /friends {list} ou /friends {request, accept, refuse, add, remove} [name]" << std::endl;
+    }
 }
