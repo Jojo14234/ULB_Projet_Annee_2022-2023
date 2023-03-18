6878c6757655ae3eb1606139a57f24aec7ffc625
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-03-18 22:51:08 +0100

    new font added in asset file

--- src/newClient/UserInterface/View/AssetsPath.hpp
+++ src/newClient/UserInterface/View/AssetsPath.hpp
@@ -13,6 +13,7 @@ const std::string LOGO_PATH = "../assets/images/logo_capitalist.png";
 
 // Fonts
 const std::string ARIAL_FONT_PATH = "../assets/fonts/arial.ttf";
+const std::string SERIF_FONT_PATH = "../assets/fonts/hvd-comic-serif-pro.ttf";
 
 
 const std::string DEFAULT_FONT_PATH = ARIAL_FONT_PATH;
