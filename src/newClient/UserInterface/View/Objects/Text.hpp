cef04d6f1743d37288ac4bd543e749c7d307c0bb
Hugo <hugocharels@hotmail.com>
2023-03-18 14:33:05 +0100

    seg fault fixed

--- src/newClient/UserInterface/View/Objects/Text.hpp
+++ src/newClient/UserInterface/View/Objects/Text.hpp
@@ -10,18 +10,15 @@
 class Text : public virtual AbstractViewObject {
 
 	sf::Text text;
+	sf::Font font;
 
 public:
 
 	Text(ObjectInfo<> info, const std::string &text, const std::string &font_path, sf::Color color=sf::Color::Black) : AbstractViewObject(info) {
-		/*
-		std::cout << font_path << std::endl;
-		sf::Font font;
 		if (!font.loadFromFile(font_path)) {
 			throw std::runtime_error("Font not found");
 		}
-		*/
-		//this->text.setFont(font);
+		this->text.setFont(font);
 		this->text.setString(text);
 		this->text.setCharacterSize(info.getHeight());
 		this->text.setFillColor(color);
--- src/newClient/UserInterface/View/WelcomeUIView.hpp
+++ src/newClient/UserInterface/View/WelcomeUIView.hpp
@@ -16,7 +16,7 @@ class WelcomeUIView: public AbstractUIView {
 	// Objects
 	Image logo{ObjectInfo<>(788.f, 145.f, WINDOW_WIDTH/2.f - 788.f/2.f, WINDOW_HEIGHT/10.f), LOGO_PATH};
 	Box background{ObjectInfo<>(WINDOW_WIDTH, WINDOW_HEIGHT, 0.f, 0.f), sf::Color(98, 205, 255)};
-	Text title{ObjectInfo<>(WINDOW_WIDTH/3.f, WINDOW_HEIGHT*9/10.f, 0.f, WINDOW_HEIGHT/10.f), "YOUHOU", ARIAL_FONT_PATH};
+	Text title{ObjectInfo<>(0.f, WINDOW_HEIGHT/8.f, WINDOW_WIDTH/3.f, WINDOW_HEIGHT*5/10.f), "YOUHOU", ARIAL_FONT_PATH};
 
 public:
 
