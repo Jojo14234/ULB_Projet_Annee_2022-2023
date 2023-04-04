1b7847abf3563c382528c5dc5d2c38d86f832e4d
Hugo <hugocharels@hotmail.com>
2023-04-04 17:11:09 +0200

    hidden input class added

---
+++ src/newClient/GraphicalUserInterface/View/Objects/HiddenInputBox.hpp
@@ -0,0 +1,28 @@
+/**
+ * Project Untitled
+ */
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+#include "ObjectInfo.hpp"
+#include "AbstractViewObject.hpp"
+#include "HiddenInputText.hpp"
+#include "Box.hpp"
+
+
+class HiddenInputBox : public HiddenInputText, public Box {
+
+public:
+
+	explicit HiddenInputBox(ObjectInfo<> info, sf::Color box_color=sf::Color::Black, const std::string &font_path=DEFAULT_FONT_PATH, sf::Color text_color=sf::Color::White) :
+			AbstractViewObject(info), HiddenInputText(info, text_color, font_path), Box(info, box_color) {}
+
+	virtual void draw(sf::RenderWindow &window) const override {
+		Box::draw(window);
+		HiddenInputText::draw(window);
+	}
+
+};
