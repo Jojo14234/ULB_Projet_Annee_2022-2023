56577e77cc23c02f3c962d3a0310be70a3ad37e8
lasauce <yacoub-22-hotmail.fr>
2023-04-08 13:36:15 +0200

    ajout du button retour dans rank

--- src/newClient/GraphicalUserInterface/View/RankGUIView.hpp
+++ src/newClient/GraphicalUserInterface/View/RankGUIView.hpp
@@ -9,10 +9,10 @@ class RankGUIController; // forward declaration
 
 class RankGUIView: public AbstractGUIView {
 
-	// Texts
+	//Texts
 	Text rank {ObjectInfo<>(WINDOW_WIDTH/4.f, WINDOW_WIDTH/4.f * 145.f/788.f, (WINDOW_WIDTH/2.f- ((WINDOW_WIDTH/4.f)/3)) , WINDOW_HEIGHT/10.f), "Ranking"};
 
-		//top 5
+		// top 5
 	Text player1 {ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50, WINDOW_WIDTH/3.f + 60, WINDOW_HEIGHT/2.f - 50), "Player12345678912345678", WHITE};
 	Text player2 {ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50, WINDOW_WIDTH/3.f + 60, WINDOW_HEIGHT/2.f + 30), "Player12345678912345678", WHITE};
 	Text player3 {ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50, WINDOW_WIDTH/3.f + 60, WINDOW_HEIGHT/2.f + 110), "Player12345678912345678", WHITE};
@@ -24,23 +24,26 @@ class RankGUIView: public AbstractGUIView {
 	Text score4 {ObjectInfo<>(WINDOW_WIDTH/60, WINDOW_WIDTH/60, WINDOW_WIDTH/2.f + 210, WINDOW_HEIGHT/2.f + 195), "123456", WHITE};
 	Text score5 {ObjectInfo<>(WINDOW_WIDTH/60, WINDOW_WIDTH/60, WINDOW_WIDTH/2.f + 210, WINDOW_HEIGHT/2.f + 275), "123456", WHITE};
 
-		//my rank
+		// my rank
 	Text my_player {ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50, WINDOW_WIDTH/3.f + 60, WINDOW_HEIGHT/2.f - 50), "PlayerMyRank", WHITE};
 	Text my_score {ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50, WINDOW_WIDTH/2.f + 210, WINDOW_HEIGHT/2.f - 45), "1", WHITE};
 	Text my_top {ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50, WINDOW_WIDTH/3.f - 15 , WINDOW_HEIGHT/2.f - 50), "100", BLACK};
 
 
-	// Boxes
+	//Boxe
 	Box box {ObjectInfo<>(WINDOW_WIDTH/2.f, WINDOW_WIDTH/2.f - 50 , WINDOW_WIDTH/4.f , WINDOW_HEIGHT/10.f), WHITE};
 
-	// Borders
+	//Border
 	Border box_border {box, 10, BLACK};
 	
 	//Image
 	Image top_box{ObjectInfo<>(WINDOW_WIDTH/4.f + 200, WINDOW_WIDTH/4.f + 80, WINDOW_WIDTH/3.f - 30 , WINDOW_HEIGHT/3.f), RANK_PATH};
 	Image my_rank_box{ObjectInfo<>(WINDOW_WIDTH/4.f + 200, WINDOW_WIDTH/8.f - 35, WINDOW_WIDTH/3.f - 30 , WINDOW_HEIGHT/3.f), MY_RANK_PATH};
 
-	// TextButtons
+	//Image Button
+	ImageButton back_button{ObjectInfo<>(WINDOW_WIDTH/16.f, WINDOW_HEIGHT/16.f, 410,100), BACK_BUTTON_PATH};
+
+	//TextButtons
 	// (ObjectInfo<>(width, height, x, y) de la box, ObjectInfo<>(width, height, x, y) du texte, text, text_color, background_color
 	TextButton top {ObjectInfo<>(WINDOW_WIDTH/15, WINDOW_WIDTH/40, WINDOW_WIDTH/3.f - 5 , WINDOW_HEIGHT/4.f-10), ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50 , WINDOW_WIDTH/3.f , WINDOW_HEIGHT/4.f - 10), "TOP 5", BLACK, RANK_COLOR}; 
 	TextButton my_rank {ObjectInfo<>(WINDOW_WIDTH/12 + 5, WINDOW_WIDTH/40, WINDOW_WIDTH/2.f + 140 , WINDOW_HEIGHT/4.f-10), ObjectInfo<>(WINDOW_WIDTH/50, WINDOW_WIDTH/50 , WINDOW_WIDTH/2.f + 145 , WINDOW_HEIGHT/4.f - 10), "My Rank", BLACK, RANK_COLOR};
@@ -67,6 +70,7 @@ public:
 	}
 
 	void drawMyRankBox() {
+		
 		my_rank_box.draw(*window);
 
 		my_player.draw(*window);
@@ -81,6 +85,8 @@ public:
 
 		top.draw(*window);
 		my_rank.draw(*window);
+		
+		back_button.draw(*window);
 
 
 	}
