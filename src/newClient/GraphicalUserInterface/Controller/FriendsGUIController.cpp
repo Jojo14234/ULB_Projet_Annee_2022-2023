03295dfc09557d96c54ad6a568e0034d1e85870f
Hugo <hugocharels@hotmail.com>
2023-04-08 18:46:28 +0200

    start controller

--- src/newClient/GraphicalUserInterface/Controller/FriendsGUIController.cpp
+++ src/newClient/GraphicalUserInterface/Controller/FriendsGUIController.cpp
@@ -4,5 +4,21 @@
 
 
 void FriendsGUIController::handle(sf::Event event) {
-	
+	switch(event.type) {
+		case sf::Event::MouseButtonPressed: {
+			// TODO buttons
+			if (this->view->back_button.contains(event.mouseButton.x, event.mouseButton.y)) {
+				this->new_state = STATE::MENU;
+			} else if (true) {
+				// TODO
+			} else {
+				// TODO
+			} break;
+		}
+		case sf::Event::TextEntered:
+		case sf::Event::KeyPressed: {
+			// TODO
+		}
+		default: break;
+	}
 }
