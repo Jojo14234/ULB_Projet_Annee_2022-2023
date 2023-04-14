d02765f9a727567ce81350bf6f0a59957afc1c6f
Hugo <hugocharels@hotmail.com>
2023-04-14 21:14:36 +0200

    reste une méthode a faire

--- src/newClient/GraphicalUserInterface/View/Objects/ScrollBox.hpp
+++ src/newClient/GraphicalUserInterface/View/Objects/ScrollBox.hpp
@@ -18,6 +18,12 @@ class ScrollBox: public AbstractViewObject {
 	std::vector<std::unique_ptr<Drawable>> objects;
 	short scroll=0;
 
+
+	void updateObjects() {
+
+	}
+
+
 public: 
 
 	ScrollBox(ObjectInfo<> info) : AbstractViewObject(info) {
@@ -32,20 +38,22 @@ public:
 	virtual void draw(sf::RenderWindow &window) const override {
 		for (short i=0; i<5; i++ ) {
 			if (this->scroll+i >= this->objects.size()) break; 
-			Drawable* obj = this->objects[this->scroll+i].get();
-			// TODO change y pos
-			obj->draw(window);
+			this->objects[this->scroll+i]->draw(window);
 		}
 	}
 
 
 	virtual void scrollUp() {
-		if (this->scroll < 5) this->scroll++; 
+		if (this->scroll >= 5) return;
+		this->scroll++; 
+		updateObjects();
 	}
 
 	virtual void scrollDown() {
-		if (this->scroll > 0) this->scroll--;
+		if (this->scroll <= 0) return;
+		this->scroll--;
+		updateObjects();
 	}
 
 
-}
+};
