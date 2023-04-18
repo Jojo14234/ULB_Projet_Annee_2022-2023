12dcfce4a549b4f83afd1fed621fe24482a99b21
Hugo <hugocharels@hotmail.com>
2023-04-18 13:39:01 +0200

    ça marche

--- src/newClient/GraphicalUserInterface/View/FriendsGUIView.hpp
+++ src/newClient/GraphicalUserInterface/View/FriendsGUIView.hpp
@@ -57,6 +57,8 @@ public:
 		this->scroll.addObject(new FriendBox{ObjectInfo<>(size_x, size_y, pos_x, pos_y+(10+size_y)*3), "__t0rtUe._.m4UdiT3__"});
 		this->scroll.addObject(new FriendBox{ObjectInfo<>(size_x, size_y, pos_x, pos_y+(10+size_y)*4), "x3l0w0"});
 		this->scroll.addObject(new FriendBox{ObjectInfo<>(size_x, size_y, pos_x, pos_y+(10+size_y)*5), "HUGOGO"});
+		this->scroll.addObject(new FriendBox{ObjectInfo<>(size_x, size_y, pos_x, pos_y+(10+size_y)*5), "youhou"});
+		this->scroll.setMaxObjToShow(6);
 	}
 	
 	~FriendsGUIView()=default;
