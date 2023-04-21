fc35fb8ac88e6009ccdedc2f7a5b9900ab9ae7ff
XEL0 <axelyx@gmail.com>
2023-04-21 17:27:42 +0200

    fix sound

--- src/newClient/GraphicalUserInterface/View/Audibles/Ambiancer.hpp
+++ src/newClient/GraphicalUserInterface/View/Audibles/Ambiancer.hpp
@@ -6,7 +6,7 @@
 class Ambiancer{
     sf::Music music;
     static inline std::vector<Ambiancer*> instances;
-    static inline int volume;
+    static inline int volume = 50;
 public:
     Ambiancer(const std::string& path) { music.openFromFile(path); }
     void playMusic() { if (volume != 0) { music.setVolume(50); music.play(); music.setLoop(true);} }
