bdc97cab105acce1b8831ff4b7b9adf9c56989c2
Ryckeboer rémy <92799821+Remy-rkb@users.noreply.github.com>
2023-04-26 18:40:01 +0200

    plan

---
+++ src/newClient/Model/Client.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Client.hpp"
+#include "InputParser/MainInputParser.hpp"
+#include "InputParser/GameInputParser.hpp"
+#include "../../Server/ClientManager/QUERY.hpp"
+
+class Client {
+
+	sf::TcpSocket socket;
+	
+	void connectToServer();
+	void disconnectFromServer();
+
+	void sendPacket(sf::Packet &packet);
+
+	int game_code = 0;
+	bool is_creator = false;
+	std::string user_name;
+    // todo added from n-curse
+    bool is_my_turn = false;
+    std::string player_turn;
+
+    //todo added from terminal
+    //bool connected_to_an_account = false;
+    //bool in_game = false;
+
+public:
+
+	Client() { this->connectToServer(); }
+    ~Client() { this->socket.disconnect(); } // todo Maybe disconnect from server via disconnectFromServer
+
+	// Send Queries
+	void sendQuery(QUERY_TYPE query);
+	void sendLogin(const std::string &username, const std::string &password);
+	void sendRegister(const std::string &username, const std::string &password);
+    void sendDisconnect();
+	void sendJoinGame(int code);
+	bool sendCommand(MainInputParser &parser); // todo == sendToServer in terminal
+	bool sendCommand(GameInputParser &parser); // todo == sendToServer in terminal
+    bool sendCommand(MainInputParser &&parser);
+    bool sendCommand(GameInputParser &&parser);
+
+	void disconnectAccount();
+
+	QUERY receive();
+	QUERY receive(std::string &output);
+
+	void receiveFriendsInfo(std::vector<std::string> &friends_name, std::vector<std::string> &friends_requests);
+
+	void setGameCode(int gc) { game_code = gc; }
+	int getGameCode() const { return game_code; }
+
+	void createGame() { is_creator = true; }
+	void leaveGame() { is_creator = false; } 
+	bool isCreator() const { return is_creator; }
+    // todo added from n-curse
+    bool isMyTurn() const { return is_my_turn; }
+    void startTurn() { is_my_turn = true; }
+    void endTurn() { is_my_turn = false; }
+    std::string getPlayerTurn() const { return player_turn; }
+    void setPlayerTurn(const std::string &player) { player_turn = player; }
+	
+	const std::string &getUsername() { return user_name; }
+
+};
